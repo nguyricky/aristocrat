ab58e6206658b199cfa61c12c151f04210b370eb
Ricky Nguyen <nguyricky@gmail.com>
2023-11-25 19:54:36 -0800

    fix: typo

--- website/src/components/pages/build.jsx
+++ website/src/components/pages/build.jsx
@@ -6,7 +6,7 @@ const Build = () => {
             <img src={logo} alt="aristocrat" className="w-1/5 h-1/5"/>
             <h2 className="text-6xl text-accent font-semibold my-24">Work in Progress...</h2>
             <p className="text-lg text-accent font-semibold">Our exciting new website will launch soon.</p>
-            <p className="text-lg text-accent font-semibold">come back and check it out.</p>  
+            <p className="text-lg text-accent font-semibold">Come back and check it out.</p>  
         </div>
     );
 };
